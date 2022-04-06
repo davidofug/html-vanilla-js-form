20a9cff9b86498b4666fecce00739d6c83bc3d58
David Wampamba <davidwampamba@gmail.com>
2022-04-06 15:50:36 +0300

    script.js updated

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const sendMessage = async (event) => {
 	event.preventDefault();
 	let sender = messages.name.value;
 	let message = messages.message.value;
-
 	try {
 		let results = await fetch("/messages", {
 			method: "POST",
@@ -15,16 +14,32 @@ const sendMessage = async (event) => {
 				message: message,
 			}),
 		});
-
+		document.querySelector(".container").style.display = "none";
 		let data = await results.json();
 		messages.message.value = "";
 		messages.name.value = "";
 		console.log(data);
-
 	} catch (err) {
 		console.log(err);
 	}
-
-    console.log(`Full name ${sender} Message ${message}`);
-    alert(`Full name ${sender} \r\nMessage ${message}`);
+	thankYou()
+	console.log(`Full name ${sender} Message ${message}`);
+	// alert(`Full name ${sender} \r\nMessage ${message}`);
 };
+
+function thankYou() {
+	const thankyou = document.createElement("div");
+	thankyou.classList.add("thank-you");
+	const thankYouHeading = document.createElement(
+		"h1"
+	);
+	thankYouHeading.innerText = "Thank you for your submission!"
+	const thankYouBody = document.createElement(
+		"p"
+	);
+	thankYouBody.innerText = "We will be in touch with you shortly."
+	thankyou.appendChild(thankYouHeading);
+	thankyou.appendChild(thankYouBody);
+
+	document.body.appendChild(thankyou);
+}
